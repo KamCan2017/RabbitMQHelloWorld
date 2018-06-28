a788a0fd1af719fcea529d47063976c929432dcb
Ngounou, Asty Noukeu <AstyNoukeu.Ngounou@cellent.de>
2018-06-28 11:42:44 +0200

    Projects added

---
+++ Common/MessageEnCoder.cs
@@ -0,0 +1,26 @@
+﻿using System.Text;
+
+namespace Common
+{
+    /// <summary>
+    /// MessageEncoder codes/decodes message for receiver and publisher
+    /// </summary>
+    public class MessageEnCoder
+    {
+        public static object CodeMessage(string message, string key)
+        {
+            if(key == RoutingKeys.HelloChannel)
+               return Encoding.UTF8.GetBytes(message);
+
+            return null;
+        }
+
+        public static object DecodeMessage(byte[] body, string key)
+        {
+            if (key == RoutingKeys.HelloChannel)
+                return Encoding.UTF8.GetString(body);
+
+            return null;
+        }
+    }
+}
---
+++ Consumer/Receive.cs
@@ -0,0 +1,45 @@
+﻿using Common;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+namespace Consumer
+{
+    class Receive
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.QueueDeclare(queue: RoutingKeys.HelloChannel,
+                                         durable: false,
+                                         exclusive: false,
+                                         autoDelete: false,
+                                         arguments: null);
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = MessageEnCoder.DecodeMessage(body, RoutingKeys.HelloChannel);
+                        Console.WriteLine(" [x] Received {0}", message);
+                    };
+
+                    channel.BasicConsume(queue: RoutingKeys.HelloChannel,
+                                         autoAck: true,
+                                         consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+
+                }
+            }
+        }
+    }
+
+    
+}
